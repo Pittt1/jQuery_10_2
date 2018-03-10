5e61618dffd407f84af3659e237030e439426d7a
Piotr Sławek <piotr.slawek@o2.pl>
2018-03-10 07:50:06 +0100

    Add content in index.html and script.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var span = $("span");
+	span.each(function(index, element) {
+		if(index % 2 == 0) {
+			$(element).css('color', 'blue');
+		};
+	});
+
+var paragraphs = $('p');
+	paragraphs.each(function(index, element) {
+		var button = '<button class="btn" data-tmp="' + index + '">Click, click..</button>'
+		$(element).append(button)
+	
+	});
+
+$("button").click(function(){
+	alert($(this).attr("data-tmp"));
+});
